3b515f02ee745431249f4d6fba3d42b6561ddff8
Somya <isomya13@gmail.com>
2023-08-31 00:41:33 +0530

    added first task

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ truffle init
 ```
 npm install
 ```
+
+**Flow of the project
+
+1.List tasks in the smart contract
+2.List tasks in the console
+3.List tasks in the client side application
+4.List tasks in the test
--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,23 @@ pragma solidity >=0.4.24;
 
 contract TodoList {
    uint public taskCount = 0;
+
+   struct Task{
+    uint id;
+    string content;
+    bool completed;
+   }
+
+   mapping(uint => Task) public tasks;
+
+   constructor() public{
+    createTask("Learn Blockchain");
+   }
+
+   function createTask(string memory _content) public{
+      taskCount++;
+      tasks[taskCount]=Task(taskCount, _content, false);
+   }
+
+
 }
