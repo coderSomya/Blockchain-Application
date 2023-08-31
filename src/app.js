c131ebbed6e203e6d76dc0e023b2a26c423d1419
Somya <isomya13@gmail.com>
2023-08-31 21:04:56 +0530

    adding tasks feature done

--- src/app.js
+++ src/app.js
@@ -72,7 +72,12 @@ App = {
         $('#account').html(App.account)
         App.setLoading(false)
     },
-
+    createTask: async()=>{
+      App.setLoading(true)
+      const content= $('#newTask').val()
+      await App.todoList.createTask(content, {from: App.account})
+      window.location.reload();
+    },
     setLoading: (boolean)=>{
         App.loading = boolean
         const loader = $('#loader')
