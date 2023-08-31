521ae7407c22499efb6b54b777252bd3feda7459
Somya <isomya13@gmail.com>
2023-08-31 21:27:39 +0530

    added test for toggling task completion

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -28,4 +28,14 @@ contract('TodoList', (accounts)=>{
     const event = result.logs[0].args
     console.log(event)
   })
+
+  it('toggles task completion', async()=>{
+    const result = await this.todoList
+    .toggleCompleted(1)
+    const task = await this.todoList.tasks(1)
+    assert.equal(task.completed, true)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 1)
+    assert.equal(event.completed, true)
+  })
 })
