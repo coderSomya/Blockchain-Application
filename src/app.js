cdcbbd58aa497ffcae4cbc4ba0148cfa497df2fc
Somya <isomya13@gmail.com>
2023-08-31 19:24:00 +0530

    rendering tasks in client side done

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ App = {
       await App.loadAccount()
       await App.loadContract()
       await App.render()
+      await App.renderTasks()
     },
+
+    loading: false,
 
     loadWeb3: async () => {
         if (typeof web3 !== 'undefined') {
@@ -24,7 +27,7 @@ App = {
           web3.eth.sendTransaction({/* ... */})
         } catch (error) {
           // User denied account access...
-          console.log(error)
+
         }
       }
       // Legacy dapp browsers...
@@ -42,7 +45,6 @@ App = {
     loadAccount: async()=>{
         let accounts = await web3.eth.getAccounts()
         App.account = accounts[0]
-        console.log(App.account)
     },
 
     loadContract: async () => {
@@ -54,7 +56,6 @@ App = {
             App.contracts.TodoList = TruffleContract(todoList);
             App.contracts.TodoList.setProvider(App.web3Provider);
     
-            console.log(todoList);
     
         } catch (error) {
             console.error('Error loading contract:', error);
@@ -64,7 +65,56 @@ App = {
     },
 
     render: async()=>{
+        if(App.loading){
+            return
+        }
+        App.setLoading(true)
         $('#account').html(App.account)
+        App.setLoading(false)
+    },
+
+    setLoading: (boolean)=>{
+        App.loading = boolean
+        const loader = $('#loader')
+        const content = $('#content')
+        if(boolean){
+            loader.show()
+            content.hide()
+        }
+        else{
+            loader.hide()
+            content.show()
+        }
+    },
+
+    renderTasks: async()=>{
+        const taskCount = await App.todoList.taskCount()
+
+        const $taskTemplate = $('.taskTemplate')
+      console.log(taskCount)
+        for(let i=1;i<=taskCount; i++){
+            const task = await App.todoList.tasks(i)
+            console.log(task)
+            const taskid = task[0].toNumber()
+            const taskcontent = task[1]
+            const taskcompleted = task[2]
+            const $newTaskTemplate = $taskTemplate.clone()
+            $newTaskTemplate.find('.content').html(taskcontent)
+            $newTaskTemplate.find('input')
+                            .prop('name', taskid)
+                            .prop('checked', taskcompleted)
+                            // .on('click', App.toggleCompleted)
+
+
+            if(taskcompleted){
+                $('#completedTaskList').append($newTaskTemplate)
+            }
+            else{
+                $('#taskList').append($newTaskTemplate)
+            }
+
+            $newTaskTemplate.show()
+        }
     }
     
 }
